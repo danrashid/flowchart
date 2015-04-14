9685aaaa291436275c52f76939305c09f21e4411
Dan Rashid <drashid@signal.co>
2015-04-14 15:32:46 -0500

    Adding download route

--- routes/charts.js
+++ routes/charts.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   router = express.Router(),
-  model = require('../models/charts');
+  model = require('../models/charts'),
+  jade = require('jade');
 
 router.get('/', function(req, res, next) {
   var charts = [];
@@ -35,6 +36,20 @@ router.get('/:id', function(req, res, next) {
       title: chart.name,
       chart: chart
     });
+  });
+});
+
+router.get('/:id/svg', function(req, res, next) {
+  model.get(req, function (chart) {
+    if (!chart) {
+      return res.status(404).send('Sorry, we cannot find that!');
+    }
+
+    res
+      .type('image/svg+xml')
+      .render('xml', {
+        svg: '<rect x="50" y="50" width="200" height="100" fill="blue" />'
+      });
   });
 });
 
