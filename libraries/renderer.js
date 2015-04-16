48d733a2dca4781e2d0896884736dca37a794a0c
Dan Rashid <drashid@signal.co>
2015-04-16 14:31:30 -0500

    Adding renderer

--- libraries/renderer.js
+++ libraries/renderer.js
@@ -1,30 +1,67 @@
+var jade = require('jade'),
+  path = require('path');
+
+function getTemplate(element) {
+  return path.join(__dirname, '../views/elements', element.type + '.jade');
+}
+
 function renderConnectors(elements) {
-  var svg = '';
+  var svg = '',
+    types = [
+      'horizontalLine',
+      'horizontalNo',
+      'horizontalYes',
+      'verticalLine',
+      'verticalNo',
+      'verticalYes'
+    ];
 
-  elements
-    .filter(function (row, i) {
-      return i % 2 === 1;
-    })
-    .forEach(function (row, yOffset) {
-      row.forEach(function (element, xOffset) {
+  elements.forEach(function (row, yOffset) {
+    row.forEach(function (element, xOffset) {
+      if (types.indexOf(element.type) > -1) {
+        var template = getTemplate(element);
 
-      });
+        svg += jade.renderFile(template, {
+          x: xOffset * 400,
+          y: yOffset * 300
+        });
+      }
     });
+  });
 
   return svg;
 }
 
 function renderShapes(elements) {
-  var svg = '';
+  var svg = '',
+    types = [
+      'decision',
+      'feedback',
+      'process'
+    ];
+
+  elements.forEach(function (row, yOffset) {
+    if (yOffset % 2 === 0) {
+      row.forEach(function (element, xOffset) {
+        if (types.indexOf(element.type) > -1) {
+          var template = getTemplate(element);
+
+          svg += jade.renderFile(template, {
+            x: xOffset * 400,
+            y: yOffset * 300,
+            text: element.match.substr(1, element.match.length - 2)
+          });
+        }
+      });
+    }
+  });
 
   return svg;
 }
 
 exports.render = function (elements) {
-  var svg = '';
-
-  svg += renderConnectors(elements);
-  svg += renderShapes(elements);
-
-  return svg;
+  return {
+    connectors: renderConnectors(elements),
+    shapes: renderShapes(elements)
+  };
 };
